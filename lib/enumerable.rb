51ff91eefbd430ef8d6769316997fb90ff169939
Camsbury <camsbury7@gmail.com>
2014-08-10 12:29:13 -0400

    Fixed my_each to really work with hashes (can take both keys and values in the yield)

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -3,11 +3,16 @@ module Enumerable
 	def my_each
 		#Return an enumerable object if no block is given
 		return enum_for(:my_each) unless block_given?
-		#Ensure hashes are accepted by converting to arrays
-		array=self.to_a
-		#performs main loop
-		for i in 0...array.length
-			yield(array[i])
+		if self.is_a?(Array)
+			for i in 0...self.length
+				yield(self[i])
+			end
+		elsif self.is_a?(Hash)
+			for i in 0...self.keys.length
+				key = self.keys[i]
+				value = self.values[i]
+				yield(key,self[key])
+			end
 		end
 		return self
 	end
